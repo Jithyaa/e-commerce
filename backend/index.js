beee885e72d72cd4b14e595ecf7b1892d0a6e88f
Jithya <jithya1504@gmail.com>
2024-07-11 00:33:21 +0530

    Add user signup with hashed password and token generation

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connect from './database/connect.js';
+import authRoutes from './routes/authRoutes.js'
 
 dotenv.config()
 const app=express()
 
-app.use(cors());
-app.use(express.json())
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials:true,
+// }));
+app.use(express.json());
 
 
 const port = process.env.PORT || 5000
 
-app.get('/',(req,res)=>{
-    res.send("jjjjjjjjjj");
-});
+app.use("/api/auth",authRoutes)
 
 app.listen(port,()=>{
     connect();
