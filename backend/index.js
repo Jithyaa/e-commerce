1e9e143fe4db5f17a756ad46a689c9f41e85d901
Jithya <jithya1504@gmail.com>
2024-07-11 08:47:30 +0530

    user authentication completed

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ import authRoutes from './routes/authRoutes.js'
 dotenv.config()
 const app=express()
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials:true,
-// }));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true,
+}));
 app.use(express.json());
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import RoutesConfig from './routes/RoutesConfig'
 
 const App=()=>{
-  return <RoutesConfig/>
+  return (
+    <div>
+      <ToastContainer />
+      <RoutesConfig />
+    </div>
+  );
 }
 
 export default App
